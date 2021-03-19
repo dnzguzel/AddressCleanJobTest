9b7e0c1f6b136811d39b73a68616931ff2c6cfd9
denizguzel1 <deniz.guzel@hepsijet.com>
2021-03-19 15:48:52 +0300

    changing

--- src/test/java/org/example/StepImplementation.java
+++ src/test/java/org/example/StepImplementation.java
@@ -3,54 +3,18 @@ package org.example;
 import com.thoughtworks.gauge.Step;
 import com.thoughtworks.gauge.Table;
 import com.thoughtworks.gauge.TableRow;
-import java.util.HashSet;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Assert;
 
 public class StepImplementation {
 
-    private HashSet<Character> vowels;
-
-    @Step("Vowels in English language are <vowelString>.")
-    public void setLanguageVowels(String vowelString) {
-        vowels = new HashSet<>();
-        for (char ch : vowelString.toCharArray()) {
-            vowels.add(ch);
-        }
-    }
-
-    @Step("The word <word> has <expectedCount> vowels.")
-    public void verifyVowelsCountInWord(String word, int expectedCount) {
-        int actualCount = countVowels(word);
-        assertThat(expectedCount).isEqualTo(actualCount);
-    }
-
-    @Step("Almost all words have vowels <wordsTable>")
-    public void verifyVowelsCountInMultipleWords(Table wordsTable) {
-        for (TableRow row : wordsTable.getTableRows()) {
-            String word = row.getCell("Word");
-            int expectedCount = Integer.parseInt(row.getCell("Vowel Count"));
-            int actualCount = countVowels(word);
-
-            assertThat(expectedCount).isEqualTo(actualCount);
-        }
-    }
-
-    private int countVowels(String word) {
-        int count = 0;
-        for (char ch : word.toCharArray()) {
-            if (vowels.contains(ch)) {
-                count++;
-            }
-        }
-        return count;
-    }
     @Step("Read from cvs file <table>")
     public void readFromCsvFile(Table table) {
         Object liste[] = new Object[3];
         for (TableRow row : table.getTableRows()) {
-            String name = row.getCell("name");
-            String surname = row.getCell("surname");
-            System.out.println("Merhaba "+name+" "+surname);
+            String value = row.getCell("value");
+            System.out.println("Adres: "+value);
+            Assert.assertFalse("Address is not same with into the csv file!",
+                    value.equals("Istasyon mahallesi dere sokak no:41"));
         }
     }
 }
